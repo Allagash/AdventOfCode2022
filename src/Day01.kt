80535fdc1695efee330bd8b13578da195683aa21
Jeff Somers <allagash@gmail.com>
2022-12-01 07:30:58 -0500

    Day 01, Calorie Counting

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,43 @@
+import java.lang.Long.max
+
+// Day 01, Advent of Code 2022, Calorie Counting
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        var max = -1L
+        var count = 0L
+        input.forEach {
+            if (it.isEmpty()) {
+                max = max(count ,max)
+                count = 0
+            } else {
+                count += it.toInt()
+            }
+        }
+        max = max(count ,max)
+        return max
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val calCount = mutableListOf<Long>()
+        var count = 0L
+        input.forEach {
+            if (it.isEmpty()) {
+                calCount.add(count)
+                count = 0
+            } else {
+                count += it.toInt()
+            }
+        }
+        return calCount.sortedDescending().subList(0, 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInput("Day01_test")
+    //check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    println(part1(input)) // 68787
+    println(part2(input)) // 198041
 }
