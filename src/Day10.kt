ce7462db01625e3812fc340b86ec6e5598344197
Jeff Somers <allagash@gmail.com>
2022-12-10 10:04:32 -0500

    Day 10, Cathode-Ray Tube, Part 1 ok, Part 2 works but has off-by 1

---
+++ src/Day10.kt
@@ -0,0 +1,70 @@
+import java.io.File
+
+// Advent of Code 2022, Day 10, Cathode-Ray Tube
+
+fun main() {
+
+    fun readInput(name: String) = File("src", "$name.txt")
+        .readLines()
+
+    fun part1(input: List<String>): Int {
+        val importantCycles = listOf(20, 60, 100, 140, 180, 220)
+        var sum = 0
+        var cycle = 1
+        var regX = 1
+        input.forEach {
+            if (cycle in importantCycles) sum += cycle * regX
+            val instruction = it.split(" ")
+            if (instruction[0] == "addx") {
+                cycle++
+                if (cycle in importantCycles) sum += cycle * regX
+                regX += instruction[1].toInt()
+            }
+            cycle++
+        }
+        return sum
+    }
+
+
+    fun part2(input: List<String>) {
+        val crt = mutableListOf<String>()
+        var line = ""
+        var cycle = 1
+        var regX = 1
+        var lineIdx = 0
+        input.forEach {
+            line += if (lineIdx in regX-1..regX+1) "#" else "."
+
+            val instruction = it.split(" ")
+            if (instruction[0] == "addx") {
+                cycle++
+                lineIdx++
+                line += if (lineIdx in regX-1..regX+1) "#" else "."
+                if (lineIdx >= 40) {
+                    crt.add(line)
+                    line = ""
+                    lineIdx = 0
+                }
+                regX += instruction[1].toInt()
+            }
+            cycle++
+            lineIdx++
+            if (lineIdx >= 40) {
+                crt.add(line)
+                line = ""
+                lineIdx = 0
+            }
+        }
+        println("CRT is:")
+        crt.forEach { println(it) }
+        println()
+    }
+
+    val testInput = readInput("Day10_test")
+    check(part1(testInput) == 13140)
+    part2(testInput)
+
+    val input = readInput("Day10")
+    println(part1(input))
+    part2(input)
+}
