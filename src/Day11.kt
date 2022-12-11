87c9ef251fd15a8f9af0bcf4bbbac3122a4bbbf4
Jeff Somers <allagash@gmail.com>
2022-12-11 14:37:45 -0500

    Day 11, Monkey in the Middle, shortened parsing & reformatted code

--- src/Day11.kt
+++ src/Day11.kt
@@ -6,46 +6,36 @@ fun main() {
 
     fun readInputAsOneLine(name: String) = File("src", "$name.txt").readText().trim()
 
-    data class Monkey(val items: MutableList<Long>, val operation: List<String>, val test: Int, val monkeys: List<Int>)
+    data class Monkey(
+        val items: MutableList<Long>,
+        val operation: List<String>,
+        val test: Int,
+        val monkeyTrue: Int,
+        val monkeyFalse: Int
+    )
 
-    fun parse(input: String) : List<Monkey> {
+    fun parse(input: String): List<Monkey> {
         val output = mutableListOf<Monkey>()
-
         val monkeyInput = input.split("\n\n")
         monkeyInput.forEach { it ->
-            var items = listOf<Long>()
-            var op = listOf<String>()
-            var test = 0
-            val monkeys = mutableListOf<Int>()
-            val lines = it.split("\n").map{it.trim()}
-            lines.forEach {
-                when {
-                    it.startsWith("Starting items:") -> {
-                        items = it.substringAfter(":").split(",").map{j -> j.trim().toLong()}
-                    }
-                    it.startsWith("Operation:") -> {
-                        op = it.substringAfter("Operation: new = old ").split(" ")
-                    }
-                    it.startsWith("Test: divisible by ") -> {
-                        test = it.substringAfter("Test: divisible by ").toInt()
-                    }
-                    it.startsWith("If true: throw to monkey ") -> {
-                        monkeys.add(0, it.substringAfter("If true: throw to monkey ").toInt())
-                    }
-                    it.startsWith("If false: throw to monkey ") -> {
-                        monkeys.add(it.substringAfter("If false: throw to monkey ").toInt())
-                    }
-                }
-            }
-            output.add(Monkey(items.toMutableList(), op, test, monkeys))
+            val lines = it.split("\n").map { it.trim() }
+            output.add(
+                Monkey(
+                    lines[1].substringAfter(":").split(",").map { j -> j.trim().toLong() }.toMutableList(),
+                    lines[2].substringAfter("Operation: new = old ").split(" "),
+                    lines[3].substringAfter("Test: divisible by ").toInt(),
+                    lines[4].substringAfter("If true: throw to monkey ").toInt(),
+                    lines[5].substringAfter("If false: throw to monkey ").toInt()
+                )
+            )
         }
         return output
     }
 
-    fun applyOp(level: Long, op: List<String>) : Long {
+    fun applyOp(level: Long, op: List<String>): Long {
         var output = level
         val second = if (op[1] == "old") level else op[1].toLong()
-        when(op[0]) {
+        when (op[0]) {
             "*" -> output *= second
             "+" -> output += second
             else -> check(false)
@@ -53,19 +43,19 @@ fun main() {
         return output
     }
 
-    fun solve(monkeys: List<Monkey>, part1: Boolean) : Long {
-        val modVal = monkeys.fold(1L) { acc, monkey ->  acc * monkey.test.toLong()}
+    fun solve(monkeys: List<Monkey>, part1: Boolean): Long {
+        val modVal = monkeys.fold(1L) { acc, monkey -> acc * monkey.test.toLong() }
         val rounds = if (part1) 20 else 10_000
         val inspections = LongArray(monkeys.size)
 
         repeat(rounds) {
-            monkeys.forEachIndexed() {i, monkey->
+            monkeys.forEachIndexed() { i, monkey ->
                 inspections[i] = inspections[i] + monkey.items.size
                 while (monkey.items.isNotEmpty()) {
                     val item = monkey.items.removeFirst()
                     var newLevel = applyOp(item, monkey.operation)
                     newLevel = if (part1) newLevel / 3 else newLevel % modVal
-                    val newMonkey = if (newLevel % monkey.test == 0L) monkey.monkeys[0] else monkey.monkeys[1]
+                    val newMonkey = if (newLevel % monkey.test == 0L) monkey.monkeyTrue else monkey.monkeyFalse
                     monkeys[newMonkey].items.add(newLevel)
                 }
             }
@@ -74,7 +64,7 @@ fun main() {
         return inspections
             .sortedDescending()
             .take(2)
-            .reduce { acc, l ->  acc * l}
+            .reduce { acc, l -> acc * l }
     }
 
     var testInput = parse(readInputAsOneLine("Day11_test"))
