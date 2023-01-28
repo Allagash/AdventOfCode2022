36c2d4d3426be067bb5152ec96e3f6a05df28a3d
Jeff Somers <allagash@gmail.com>
2023-01-28 12:55:06 -0500

    Day 17, Part 2 works with test input

--- src/Day17.kt
+++ src/Day17.kt
@@ -1,6 +1,4 @@
 import java.io.File
-import kotlin.math.abs
-import kotlin.math.min
 import kotlin.math.max
 
 // Advent of Code 2022, Day 17, Pyroclastic Flow
@@ -43,6 +41,8 @@ class Rock5 : Rock {
 
 data class Offset(var x: Int, var y: Int)
 
+data class Point(var x: Int, var y: Int)
+
 val shapes = listOf(Rock1(), Rock2(), Rock3(), Rock4(), Rock5())
 
 fun main() {
@@ -78,9 +78,8 @@ fun main() {
         var highestY = -1
         var moveIdx = -1
 
-        //!FIX 3rd one screws up - needs to go down one more
         repeat(2022) {i ->
-            var rock = shapes[i % shapes.size]
+            val rock = shapes[i % shapes.size]
             var pt = Offset(2, highestY + 4)
             while (true) {
                 moveIdx = (moveIdx+1) % input.length
@@ -109,16 +108,86 @@ fun main() {
         return highestY + 1
     }
 
-    fun part2(input: String): Int {
+    class Day17(input: String) {
+        val jets = input
 
-        return 0
+        val rock0 = listOf(Point(0, 0), Point(1, 0), Point(2, 0), Point(3, 0))
+        val rock1 = listOf(Point(1, 0), Point(0, 1), Point(1, 1), Point(2, 1), Point(1, 2))
+        val rock2 = listOf(Point(0, 0), Point(1, 0), Point(2, 0), Point(2, 1), Point(2,2))
+        val rock3 = listOf(Point(0, 0), Point(0, 1), Point(0, 2), Point(0, 3))
+        val rock4 = listOf(Point(0, 0), Point(1, 0), Point(0, 1), Point(1, 1))
+        val rocks = listOf(rock0, rock1, rock2, rock3, rock4)
+
+        fun printWorld(highestY: Int, world: Set<Point>) {
+            for (y in highestY + 4 downTo 0) {
+                print('|')
+                for (x in 0..6) {
+                    val c = if (Point(x, y) in world) '#' else '.'
+                    print(c)
+                }
+                println('|')
+            }
+            println("+-------+\n")
+        }
+
+        fun intersect(pt: Point, rock: List<Point>, world: Set<Point>): Boolean {
+            rock.forEach {
+                val x = it.x + pt.x
+                val y = it.y + pt.y
+                if (x < 0 || y < 0 || x >= 7) return true // >= 7 or just >?
+
+                if (Point(x,y) in world) return true
+            }
+            return false
+        }
+
+        fun findHeight() : Long {
+            var highestY = -1
+            var moveIdx = -1
+            val world = mutableSetOf<Point>()
+
+            repeat(2022) {i ->
+                val rock = rocks[i % rocks.size]
+                var pt = Point(2, highestY + 4)
+                while (true) {
+                    moveIdx = (moveIdx+1) % jets.length
+                    check(jets[moveIdx] == '<' || jets[moveIdx] == '>')
+                    val push = if (jets[moveIdx] == '<') -1 else 1
+                    var nextPt = Point(pt.x + push, pt.y)
+                    if (!intersect(nextPt, rock, world)) {
+                        pt = nextPt
+                    }
+                    nextPt = Point(pt.x, pt.y - 1)
+                    if (intersect(nextPt, rock, world)) {
+                        break
+                    }
+                    pt = nextPt
+                }
+                // freeze it
+                rock.forEach {
+                    val x = it.x + pt.x
+                    val y = it.y + pt.y
+                    world.add(Point(x,y))
+                    highestY = max(highestY, y)
+                }
+//                printWorld(highestY, world)
+            }
+
+            return highestY + 1L
+        }
+
+    }
+
+    fun part2(input: String): Long {
+        val d = Day17(input)
+        return d.findHeight()
     }
 
     val testInput = readInputAsOneLine("Day17_test")
-    println(part1(testInput))
+    check(part1(testInput)==3068)
+    println("${part2(testInput)} should equal 3068")
 
     val input = readInputAsOneLine("Day17")
     println(part1(input))
-//    println(part1(input, 2_000_000))
 //    println(part2(input, 4_000_000))
 }
